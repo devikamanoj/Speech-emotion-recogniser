938a9ee0ff0d97f42d0d6fe86bace74c6a85dc18
Devika <dmenon138@gmail.com>
2023-05-25 10:57:16 +0530

    Update ser.py

--- ser.py
+++ ser.py
@@ -1,9 +1,11 @@
 #  Train the model wrt dataset
 
 from sklearn.neural_network import MLPClassifier #to make model
-from sklearn.metrics import accuracy_score, f1_score, precision_score, plot_confusion_matrix #for evaluation metrices
-import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, precision_score,confusion_matrix
+import numpy as np
+import itertools
 
+import matplotlib.pyplot as plt
 import os
 import pickle
 
@@ -33,7 +35,7 @@ model_params = {
     'epsilon': 1e-08,
     'hidden_layer_sizes': (300,),
     'learning_rate': 'adaptive',
-    'max_iter': 500,
+    'max_iter': 600,
 }
 # using best parameters so far, init MLP classifier
 model = MLPClassifier(**model_params)
@@ -64,6 +66,30 @@ if not os.path.isdir("result"):
 # for future use, the model is saved using pickle module
 pickle.dump(model, open("result/mlp_classifier.model", "wb"))
 
-plot_confusion_matrix(model, X_test, y_test,cmap=plt.cm.Blues, normalize='true')
-plt.title('Confusion matrix for the classifier')
+# Predict the labels for the test set
+y_pred = model.predict(X_test)
+
+# Compute the confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Plot the confusion matrix
+plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title('Confusion Matrix')
+plt.colorbar()
+classes = np.unique(y_test)
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes)
+plt.yticks(tick_marks, classes)
+
+# Label the plot
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, cm[i, j],
+             horizontalalignment="center",
+             color="white" if cm[i, j] > thresh else "black")
+
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.tight_layout()
 plt.show()
+
